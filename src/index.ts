e032729b9e109421adc7e1834edef09627a83c63
Nick Zou <nick@elevate.ca>
2023-05-20 18:56:15 -0400

    add todoist-api and get projects and createmilk calls

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello typescript!");
+import * as dotenv from "dotenv";
+import { TodoistApi } from "@doist/todoist-api-typescript";
+
+dotenv.config();
+
+const api = new TodoistApi("177d3de82208c3098076935a7ea33eedc1be75a5");
+
+api
+  .getProjects()
+  .then((projects) => {
+    console.log(projects);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+api
+  .addTask({ content: "Buy Milk", projectId: "2078702594" })
+  .then((task) => console.log(task))
+  .catch((error) => console.log(error));
