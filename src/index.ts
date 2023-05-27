48beac67ecf17c96c5d4781de41ddde36e317e55
Nick Zou <nick@elevate.ca>
2023-05-27 18:03:25 -0400

    update to default

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,15 @@ import { TodoistApi } from "@doist/todoist-api-typescript";
 
 dotenv.config();
 
-setInterval(() => {
-  const api = new TodoistApi("177d3de82208c3098076935a7ea33eedc1be75a5");
-  api
-    .addTask({ content: "Buy Milk", projectId: "2078702594" })
-    .then((task) => console.log(task))
-    .catch((error) => console.log(error));
-}, 120000);
+console.log("hello world");
+
+// setInterval(() => {
+//   const api = new TodoistApi("177d3de82208c3098076935a7ea33eedc1be75a5");
+//   api
+//     .addTask({ content: "Buy Milk", projectId: "2078702594" })
+//     .then((task) => console.log(task))
+//     .catch((error) => console.log(error));
+// }, 120000);
 
 // api
 //   .getProjects()
