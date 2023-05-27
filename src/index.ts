3ecf1ff9b482d8323008b37e0a18c77a3df2b918
Nick Zou <nick@elevate.ca>
2023-05-27 18:15:22 -0400

    add default route

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ const port = 3000;
 
 const app = express();
 
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
