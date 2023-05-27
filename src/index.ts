5e6d46e6b133a9ff1245cef7a1d98dce7e70effe
Nick Zou <nick@elevate.ca>
2023-05-27 19:48:50 -0400

    add cron

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import express, { Request, Response } from "express";
 // import * as dotenv from "dotenv";
 import { TodoistApi } from "@doist/todoist-api-typescript";
+import cron from "node-cron";
 
 const port = 3000;
 
 //dotenv.config();
 
 const app = express();
+const api = new TodoistApi("177d3de82208c3098076935a7ea33eedc1be75a5");
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
+});
+
+cron.schedule("*/1 * * * *", () => {
+  api
+    .addTask({ content: "Buy Milk", projectId: "2078702594" })
+    .then((task) => console.log(task))
+    .catch((error) => console.log(error));
 });
 
 app.listen(port, () => {
@@ -24,9 +33,3 @@ app.listen(port, () => {
 //   .catch((err) => {
 //     console.log(err);
 //   });
-
-// const api = new TodoistApi("177d3de82208c3098076935a7ea33eedc1be75a5");
-//   api
-//     .addTask({ content: "Buy Milk", projectId: "2078702594" })
-//     .then((task) => console.log(task))
-//     .catch((error) => console.log(error));
