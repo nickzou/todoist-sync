e751bec01523ee8b0df851c403235d4840b50ca2
Nick Zou <nick@elevate.ca>
2023-05-27 21:05:45 -0400

    ad google service account to gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,43 @@
+//Access to environment variables
 import * as dotenv from "dotenv";
 dotenv.config();
 
-import express, { Request, Response } from "express";
-import { TodoistApi } from "@doist/todoist-api-typescript";
-import cron from "node-cron";
+//Google APIs
+import { google } from "googleapis";
 
+//Express
+import express, { Request, Response } from "express";
+
+//Todoist API
+import { TodoistApi } from "@doist/todoist-api-typescript";
+
+//Other stuff I need
+import cron from "node-cron";
+//import fetch from "node-fetch";
+
+//declaring environment variables so TypeScript doesn't yell at me (or crash the app)
 declare const process: {
   env: {
     TODOIST_API_TOKEN: string;
+    GOOGLE_API_CLIENT_ID: string;
+    GOOGLE_API_CLIENT_SECRET: string;
+    GOOGLE_API_REDIRECT_URI: string;
   };
 };
 
+//declaring port
 const port = 3000;
 
+//Starting stuff
 const app = express();
-const api = new TodoistApi(process.env.TODOIST_API_TOKEN);
+const { OAuth2 } = google.auth;
+const TodoistAPI = new TodoistApi(process.env.TODOIST_API_TOKEN);
+
+const oauth2Client = new OAuth2(
+  process.env.GOOGLE_API_CLIENT_ID,
+  process.env.GOOGLE_API_CLIENT_SECRET,
+  process.env.GOOGLE_API_REDIRECT_URI
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
@@ -30,12 +53,3 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
-// api
-//   .getProjects()
-//   .then((projects) => {
-//     console.log(projects);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
