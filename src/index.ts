703af76bc383e5f63f071b4222ef744c89d0e7b9
Nick Zou <nick@elevate.ca>
2023-05-27 20:05:22 -0400

    new scripts

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,31 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import express, { Request, Response } from "express";
-// import * as dotenv from "dotenv";
 import { TodoistApi } from "@doist/todoist-api-typescript";
 import cron from "node-cron";
 
+declare const process: {
+  env: {
+    TODOIST_API_TOKEN: string;
+  };
+};
+
 const port = 3000;
 
-//dotenv.config();
-
 const app = express();
-const api = new TodoistApi("177d3de82208c3098076935a7ea33eedc1be75a5");
+const api = new TodoistApi(process.env.TODOIST_API_TOKEN);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-// cron.schedule("*/1 * * * *", () => {
-//   api
-//     .addTask({ content: "Buy Milk", projectId: "2078702594" })
-//     .then((task) => console.log(task))
-//     .catch((error) => console.log(error));
-// });
+cron.schedule("*/1 * * * *", () => {
+  api
+    .addTask({ content: "Buy Milk", projectId: "2078702594" })
+    .then((task) => console.log(task))
+    .catch((error) => console.log(error));
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
